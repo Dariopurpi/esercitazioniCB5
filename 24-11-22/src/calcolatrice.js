7905743372ec832880c7fedd57bfdd08fabf5361
Dario Purpi <dariopurpi@gmail.com>
2022-11-24 17:58:57 +0100

    esercitazione 24-11-22

---
+++ 24-11-22/src/calcolatrice.js
@@ -0,0 +1,16 @@
+// Funzioni operazioni
+function somma(num1, num2) {
+  return parseInt(num1) + parseInt(num2);
+}
+
+function sottrazione(num1, num2) {
+  return parseInt(num1) - parseInt(num2);
+}
+
+function divisione(num1, num2) {
+  return parseInt(num1) / parseInt(num2);
+}
+
+function moltiplicazione(num1, num2) {
+  return parseInt(num1) * parseInt(num2);
+}
