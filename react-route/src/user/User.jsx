389bcef50cca1842261d9f03744d8e2edd909dc3
Dario Purpi <dariopurpi@gmail.com>
2023-01-30 18:18:17 +0100

    esercitazione 30-01-23 funzionante

--- react-route/src/user/User.jsx
+++ react-route/src/user/User.jsx
@@ -3,25 +3,27 @@ import { useParams, useNavigate } from "react-router-dom";
 import styles from "./styles.module.scss";
 
 const User = () => {
-  let { userNumber } = useParams();
+  let { user } = useParams();
   const navigate = useNavigate();
   const [userList, setUserList] = useState({});
+  console.log(user);
 
   useEffect(() => {
-    fetch(`https://dummyjson.com/users/${userNumber}`)
+    fetch(`https://dummyjson.com/users/${user}`)
       .then((response) => response.json())
       .then((data) => setUserList(data))
       .then(console.log(userList));
-  }, []);
+  }, [user]);
 
-  const onClickPrevBtn = () => navigate(`/users/${userNumber + 1}`);
-  const onClickNextBtn = () => navigate(`/users/${userNumber - 1}`);
+  const onClickPrevBtn = () => navigate(`/users/${parseInt(--user)}`);
+  const onClickNextBtn = () => navigate(`/users/${parseInt(++user)}`);
 
   return (
     <div className={styles.Quote}>
-      <div className={styles.content}>
-        <h3>{userList?.firstName}</h3>
-      </div>
+      <p>{userList.id}</p>
+      <h3>{userList.firstName}</h3>
+      <img src={userList.image} alt="" />
+
       <div className={styles.buttons}>
         <button onClick={onClickPrevBtn}>Prev</button>
         <button onClick={() => navigate("/")}>To Homepage</button>
